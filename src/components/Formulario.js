7186a54e3b9985f4dcb9a21ed24325fafc56e1b1
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-09 00:17:17 -0300

    Se agregó el useState para el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Formulario = () => {
+  //state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  //extraer ciudad y pais con destructure
+  const { ciudad, pais } = busqueda;
+
+  //función que coloca los elementos en el state
+  //handleChange es una convención internacional
+  const handleChange = (e) => {
+    //actualizar el state
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="input-field col s12">
-        <input type="text" name="ciudad" id="ciudad" />
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
         <label htmlFor="ciudad">Ciudad: </label>
       </div>
 
       <div className="input-field col s12">
-        <select name="pais" id="pais">
+        <select
+          name="pais"
+          id="pais"
+          value={pais}
+          onChange={handleChange}
+        >
           <option value="">-- Seleccione un país --</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
         </select>
-        <label htmlFor="pais">País: </label>
+        {/* <label htmlFor="pais">País: </label> */}
       </div>
     </form>
   );
