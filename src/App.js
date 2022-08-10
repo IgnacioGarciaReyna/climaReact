6fc2d503e974d24ec31a6dca4cd2690337ea9645
IgnacioGarciaReyna <ignaciogarciareyna@live.com>
2022-08-09 21:21:36 -0300

    Se agregó la consulta a la API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Header from "./components/Header";
-import Formulario  from "./components/Formulario";
+import Formulario from "./components/Formulario";
 
 function App() {
+  //state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = "edd32094f88b8990e35629e37fb28530";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        console.log(resultado);
+      }
+    };
+    consultarAPI();
+  }, [consultar]);
+
   return (
     <Fragment>
       <Header titulo="Clima React App" />
       <div className="contenedor-form">
         <div className="container">
           <div className="row">
-            <div className="col m6 s12"><Formulario /></div>
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
+            </div>
             <div className="col m6 s12">2</div>
           </div>
         </div>
